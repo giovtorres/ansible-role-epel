0de5c3b7804801b0803ba6b83640eaf1f923f44e
Giovanni Torres <giovanni.torres@gmail.com>
2019-06-30 15:58:30 -0400

    Added scenario for testing epel absent

---
+++ molecule/epel-absent/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_epel_repo_conf(host):
+    f = host.file("/etc/yum.repos.d/epel.repo")
+    assert not f.exists
+    assert not f.is_file
+
+
+def test_epel_gpg_key(host):
+    if host.system_info.release.startswith("7"):
+        assert not host.package("gpg-pubkey-352c64e5-52ae6884").is_installed
+    if host.system_info.release.startswith("6"):
+        assert not host.package("gpg-pubkey-0608b895-4bd22942").is_installed
+
+
+def test_epel_repo_enabled(host):
+    cmd = "yum -v repolist epel"
+    assert "repolist: 0" in host.check_output(cmd)
